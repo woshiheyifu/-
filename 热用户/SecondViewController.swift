26dad9a1cbdbed8341a5a89c669a93a854579fae
中盛恒通 <zsenergy@zhongshenghengtongdeimac.local>
2015-11-09 10:39:06 +0800

    Initial Commit

---
+++ 热用户/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  热用户
+//
+//  Created by 中盛恒通 on 15/11/9.
+//  Copyright © 2015年 中盛恒通. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
